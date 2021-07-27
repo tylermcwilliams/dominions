16c90a00a4d1ab8ef8bb1ccf3f8680856cbcc450
Kiwin <kikoro@live.dk>
2021-07-27 15:23:06 +0200

    extract LocalTalk from switch to class

---
+++ mods/cultus/src/NPC/Utils/LocalTalk/TalkModePresets/TalkModeNormal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cultus.src.NPC.Utils.LocalTalk.TalkModePresets
+{
+    public class TalkModeNormal : ILocalTalkMode
+    {
+        public float AudiableDistance => 15;
+        public virtual string ApplyTalkModeEffectToMessage(string message)
+        {
+            return message;
+        }
+    }
+}
---
+++ mods/cultus/src/NPC/Utils/LocalTalk/TalkModePresets/TalkModeWhisper.cs
@@ -0,0 +1,12 @@
+﻿namespace cultus.src.NPC.Utils.LocalTalk.TalkModePresets
+{
+    public class TalkModeWhisper : TalkModeNormal
+    {
+        private const float WHISPER_DISTANCE_MODIFIER = 0.5f;
+        public new float AudiableDistance => base.AudiableDistance * WHISPER_DISTANCE_MODIFIER;
+        public override string ApplyTalkModeEffectToMessage(string message)
+        {
+            return message.ToLower();
+        }
+    }
+}
