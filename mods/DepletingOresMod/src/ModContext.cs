f21fdb9829d4ba875b5b1058f9ae74b6f3810153
Kiwin <kikoro@live.dk>
2021-07-27 14:49:19 +0200

    add DepletingOresMod

---
+++ mods/DepletingOresMod/src/ModContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace depletingores.src
+{
+    public static class ModContext
+    {
+        public static class ClassNames
+        {
+            public const string DepletingOreBlock = "DepletingOreBlock";
+            public const string DepletingOreEntity = "DepletingOreEntity";
+        }
+    }
+}
